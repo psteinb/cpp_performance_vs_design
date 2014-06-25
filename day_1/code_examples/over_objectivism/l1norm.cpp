5430ca2773dc26aeec8229bf9ca9bae7c45fabb2
Peter Steinbach <steinbach@scionics.de>
2014-06-25 16:10:05 +0200

    created stub for expression template demo

---
+++ day_1/code_examples/over_objectivism/l1norm.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <chrono>
+#include "stack.hpp"
+
+int main(int argc, char* argv[])
+{
+  
+  const int pixel_count_x = 256*argc; 
+  const int pixel_count_y = 256*argc; 
+  const int pixel_count_z = 256; 
+
+  size_t memory_consumption_mbyte = pixel_count_x*pixel_count_y*pixel_count_z*4/(1<<20);
+
+  flat_stack flat_first(pixel_count_x, pixel_count_y, pixel_count_z);
+  flat_stack flat_second(pixel_count_x, pixel_count_y, pixel_count_z);
+  flat_first.fill(42);
+  flat_second.fill(41);
+  
+  double flat_l1norm = 0;
+  auto t_start = std::chrono::high_resolution_clock::now();
+  for(int i = 0;i<50;++i)
+    flat_l1norm = flat_first.l1norm(flat_second);
+  auto t_end = std::chrono::high_resolution_clock::now();
+  double delta_flat = std::chrono::duration_cast<std::chrono::milliseconds>(t_end - t_start).count()/1e3;
+  std::cout << "flat		" << delta_flat  <<" s (l1norm: " << flat_l1norm << ", "<< memory_consumption_mbyte << " MB)\n";
+
+  pixel_stack pixel_first(pixel_count_x, pixel_count_y, pixel_count_z);
+  pixel_stack pixel_second(pixel_count_x, pixel_count_y, pixel_count_z);
+  pixel_first.fill(44);
+  pixel_second.fill(43);
+
+  double pixel_l1norm = 0;
+  t_start = std::chrono::high_resolution_clock::now();
+  for(int i = 0;i<50;++i)
+    pixel_l1norm = pixel_first.l1norm(pixel_second);
+  t_end = std::chrono::high_resolution_clock::now();
+  double delta_pixel = std::chrono::duration_cast<std::chrono::milliseconds>(t_end - t_start).count()/1e3;
+  std::cout << "pixel		" << delta_pixel  <<" ms (l1norm: " << pixel_l1norm << ", "<< memory_consumption_mbyte << " MB)\n";
+
+  return 0;
+}
--- day_2/03_expression_templates/acp_start.cpp
+++ day_2/03_expression_templates/acp_start.cpp
@@ -1,9 +1,17 @@
 #define ACP_TEST_CPP
 #include <iostream>
+#include "static_array.hpp"
 // file to serve as a starting point
 
 int main(int argc, char *argv[])
 {
-  std::cout << "Hello World!\n";
+  std::cout << "Expression Templates!\n";
+
+  static_array<int,1024> first(1);
+  static_array<int,1024> second(2);
+  static_array<int,1024> result = first + second;
+  
+  std::cout << "norm of first + second is " << result.norm() << "\n";
+
   return 0;
 }
